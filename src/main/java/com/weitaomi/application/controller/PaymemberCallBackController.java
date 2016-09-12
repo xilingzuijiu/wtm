c371957bb85d18cb1ec42887fadb48c79309affc
xilingzuijiu <xilingzuijiu@gmail.com>
2016-09-12 14:17:16 +0800

    F【支付】支付宝支付公网测试

--- src/main/java/com/weitaomi/application/controller/PaymemberCallBackController.java
+++ src/main/java/com/weitaomi/application/controller/PaymemberCallBackController.java
@@ -8,6 +8,7 @@ import com.thoughtworks.xstream.io.xml.XmlFriendlyNameCoder;
 import com.weitaomi.application.service.interf.IPaymentService;
 import com.weitaomi.systemconfig.alipay.AlipayNotification;
 import com.weitaomi.systemconfig.alipay.AlipayNotify;
+import com.weitaomi.systemconfig.alipay.HttpRequest;
 import com.weitaomi.systemconfig.alipay.RequestUtils;
 import com.weitaomi.systemconfig.exception.BusinessException;
 import com.weitaomi.systemconfig.exception.SystemException;
@@ -31,6 +32,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -42,47 +45,69 @@ public class PaymemberCallBackController {
     private Logger logger= LoggerFactory.getLogger(PaymemberCallBackController.class);
     @Autowired
     private IPaymentService paymentService;
-    @ResponseBody
     @RequestMapping(value = "/verifyAlipayNotify", method = RequestMethod.POST)
-    public void  verifyAlipayNotify(HttpServletRequest request,HttpServletResponse response) throws SystemException,BusinessException {
-        Map<String, String> underScoreKeyMap = request.getParameterMap();
-        Map<String, String> camelCaseKeyMap = RequestUtils.convertKeyToCamelCase(underScoreKeyMap);
-
-        //首先验证调用是否来自支付宝
-        boolean verifyResult = AlipayNotify.verify(underScoreKeyMap);
-
-        try {
-
-            String jsonString = JSON.toJSONString(camelCaseKeyMap);
-            AlipayNotification notice = JSON.parseObject(jsonString, AlipayNotification.class);
-            notice.setVerifyResult(verifyResult);
-
-            String resultResponse = "success";
-            PrintWriter printWriter = null;
-            try {
-                printWriter = response.getWriter();
-
-                //do business
-                if(verifyResult){
-                    paymentService.verifyAlipayNotify(underScoreKeyMap);
-                }
-                //fail due to verification error
-                else{
-                    resultResponse = "fail";
-                }
-
-            } catch (Exception e) {
-                logger.error("alipay notify error :", e);
-                resultResponse = "fail";
-                printWriter.close();
+    public void  verifyAlipayNotify(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        //获取支付宝POST过来反馈信息
+        Map<String,String> params = new HashMap<String,String>();
+        Map requestParams = request.getParameterMap();
+        for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext();) {
+            String name = (String) iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i]
+                        : valueStr + values[i] + ",";
             }
-            if (printWriter != null) {
-                printWriter.print(resultResponse);
+            //乱码解决，这段代码在出现乱码时使用。如果mysign和sign不相等也可以使用这段代码转化
+            //valueStr = new String(valueStr.getBytes("ISO-8859-1"), "gbk");
+            params.put(name, valueStr);
+        }
+
+        //获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以下仅供参考)//
+        //商户订单号
+
+        String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"),"UTF-8");
+
+        //支付宝交易号
+
+        String trade_no = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"),"UTF-8");
+
+        //交易状态
+        String trade_status = new String(request.getParameter("trade_status").getBytes("ISO-8859-1"),"UTF-8");
+
+        //获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以上仅供参考)//
+
+        if(AlipayNotify.verify(params)){//验证成功
+            //////////////////////////////////////////////////////////////////////////////////////////
+            //请在这里加上商户的业务逻辑程序代码
+
+            //——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
+
+            if(trade_status.equals("TRADE_FINISHED")){
+                //判断该笔订单是否在商户网站中已经做过处理
+                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                //请务必判断请求时的total_fee、seller_id与通知时获取的total_fee、seller_id为一致的
+                //如果有做过处理，不执行商户的业务程序
+                logger.info("验证签名成功");
+                //注意：
+                //退款日期超过可退款期限后（如三个月可退款），支付宝系统发送该交易状态通知
+            } else if (trade_status.equals("TRADE_SUCCESS")){
+                //判断该笔订单是否在商户网站中已经做过处理
+                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                //请务必判断请求时的total_fee、seller_id与通知时获取的total_fee、seller_id为一致的
+                //如果有做过处理，不执行商户的业务程序
+                logger.info("验证签名失败");
+                //注意：
+                //付款完成后，支付宝系统发送该交易状态通知
             }
 
-        } catch (Exception e1) {
+            //——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
 
-            e1.printStackTrace();
+            response.getOutputStream().print("success");	//请不要修改或删除
+
+            //////////////////////////////////////////////////////////////////////////////////////////
+        }else{//验证失败
+            response.getOutputStream().print("fail");
         }
     }
     @ResponseBody
