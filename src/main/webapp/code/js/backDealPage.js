f6e3f45be882e40be84840e12c932426b9ff80f5
xilingzuijiu <xilingzuijiu@gmail.com>
2016-10-24 14:22:33 +0800

    F【后台管理】申请提现页面

--- src/main/webapp/code/js/backDealPage.js
+++ src/main/webapp/code/js/backDealPage.js
@@ -1,15 +1,12 @@
 var approveHead = ' <tr class="list">' +
-    '<td><label><input class="all" type="checkbox" value="全选"/>全选</label></td>' +
     '<td>提现方式</td>' +
     '<td>用户账号</td>' +
     '<td>用户ID</td>' +
     '<td>提现时间</td>' +
     '<td>提现金额</td>' +
-    '<td>处理状态</td>' +
+    '<td>处理意见</td>' +
     '<td>审批</td>' +
     '</tr>'
-
-var total=0;
 function getApproveList(pageIndex,pageSize) {
     if (typeof pageIndex =='undefine' || pageIndex ==null){
         pageIndex=0
@@ -17,19 +14,30 @@ function getApproveList(pageIndex,pageSize) {
     if (typeof pageSize =='undefine' || pageSize ==null){
         pageSize=0
     }
+    //patchWechatCustomers
     $.ajax({
         type: 'get',
         dataType: 'json',
         url: '/pc/admin/paymemberCallBack/getApproveList',
         data: {pageIndex:pageIndex,pageSize:pageSize},
-        success: function (params) {
+        success: function (params){
             var json = eval(params); //数组
             console.log("json数据为：" + params)
-            if (json != null && json.errorCode == 0) {
-                var data = json.data
-                $("table:first").empty()
-                var trs=dealGetApproveData(data)
-                $("table:first").append(trs)
+            if (json != null && json.errorCode == 0){
+                var data = json.data;
+                $("table:first").empty();
+                var trs=dealGetApproveData(data);
+                $("table:first").append(trs);
+                pageCount=Math.ceil(total/pageSize);
+                console.log("最后是"+total);
+                $(".tcdPageCode").createPage({
+                    pageCount:pageCount,
+                    current:1,
+                    backFn:function(p){
+                        console.log("什么是"+p);
+                        getApproveList(p,pageSize);
+                    }
+                });
             } else {
                 alert("获取列表失败")
             }
@@ -38,8 +46,8 @@ function getApproveList(pageIndex,pageSize) {
 }
 
 function dealGetApproveData(data){
-    total=data.total
-    var elements=approveHead
+    total=data.total;
+    var elements=approveHead;
     data.list.forEach(function (child) {
         elements  = elements + getApproveListTr(child.payType,child.accountName,child.createTime,child.amount,child.isPaid,child.memberId)
     })
@@ -54,26 +62,76 @@ function getApproveListTr(account,name,time,amount,isCheck,memberId) {
     }
     if (isCheck==1){
         checkState='已审核'
-    }else if(isCheck==0) {
-        checkState='未审核'
+    }else if(isCheck==0){
+        checkState='拒绝'
     }
     var approvecontent = '<tr class="list">' +
-        '<td>' +
-        '<label><input class="selectchild" type="checkbox" value=""/></label>' +
+    '<td>'+payType+'</td>' +
+    '<td>'+name+'</td>' +
+    '<td>'+memberId+'</td>' +
+    '<td>'+getLocalTime(time)+'</td>' +
+    '<td>'+amount+'</td>' +
+    '<td>'+'</td>' +
+    '<td class="audit">'+
+        '<select onClick="selectSubmit(this)">'+
+        '<option id="0">'+'待审核'+'</option>'+
+        '<option id="1">'+'已审核'+'</option>'+
+        '<option id="2">'+'拒绝'+'</option>'+
+        '</select>'+
         '</td>' +
-        '<td>'+payType+'</td>' +
-        '<td>'+name+'</td>' +
-        '<td>'+memberId+'</td>' +
-        '<td>'+getLocalTime(time)+'</td>' +
-        '<td>'+amount+'</td>' +
-        '<td>'+checkState+'</td>' +
-        '<td class="audit">'+checkState+'</td>' +
-        '</tr>'
+    '</tr>'
     return approvecontent
 }
-
-
-
+var selecount=0;
+var selectSubmit=function(obj){
+    var index=$(".audit select").get(0).selectedIndex;
+    index=parseInt(index);
+    console.log("index是"+index);
+    console.log("selecount是"+selecount);
+        if(selecount!=0){
+            var view=prompt("请输入审核意见","通过");
+            $(".audit").prev().empty();
+            $(".audit").prev().text(view);
+            if(view){
+                var isCheck=0;
+                switch (index){
+                    case 0:
+                        isCheck=2;
+                        break;
+                    case 1:
+                        isCheck=1;
+                        break;
+                    case 2:
+                        isCheck=0;
+                }
+                if(isCheck<2){
+                console.log("索引号是"+index);
+                $.ajax({
+                    type: 'get',
+                    dataType: 'json',
+                    url: '/pc/admin/paymemberCallBack/patchWechatCustomers',
+                    data: {isCheck:index},
+                    success: function (params){
+                        var json = eval(params); //数组
+                        console.log("json数据为：" + params)
+                        if (json != null && json.errorCode == 0){
+                            var data = json.data;
+                            $(".audit select").css("color","green");
+                        } else {
+                            $(".audit").prev().empty();
+                            alert("获取数据失败")
+                        }
+                    },error:function(){
+                        $(".audit").prev().empty();
+                        alert("审核失败，请重新审核");
+                    }
+                })
+            }
+            selecount--;
+}}else{
+            selecount++;
+        }
+}
 function getLocalTime(nS) {
     return new Date(parseInt(nS) * 1000).toLocaleString().replace(/年|月/g, "-").replace(/日/g, " ");
 }
---
+++ src/main/webapp/code/js/page.js
@@ -0,0 +1,131 @@
+//鍒嗛〉鎻掍欢
+/**
+ 2014-08-05 ch
+ **/
+(function($){
+    var ms = {
+        init:function(obj,args){
+            return (function(){
+                ms.fillHtml(obj,args);
+                ms.bindEvent(obj,args);
+            })();
+        },
+        //濉厖html
+        fillHtml:function(obj,args){
+            return (function(){
+                obj.empty();
+                //涓婁竴椤�
+
+                if(args.current > 1){
+                    obj.append('<a href="javascript:;" class="start">首页</a>');
+                    obj.append('<a href="javascript:;" class="prevPage">&lt</a>');
+                }else{
+                    obj.remove('.prevPage');
+                    obj.remove('.start');
+                    obj.append('<span class="disabled">首页</span>');
+                    obj.append('<span class="disabled">&lt</span>');
+                }
+                //涓棿椤电爜
+                if(args.current != 1 && args.current >= 4 && args.pageCount != 4){
+                    obj.append('<a href="javascript:;" class="tcdNumber">'+1+'</a>');
+                }
+                if(args.current-2 > 2 && args.current <= args.pageCount && args.pageCount > 5){
+                    obj.append('<span>...</span>');
+                }
+                var start = args.current -2,end = args.current+2;
+                if((start > 1 && args.current < 4)||args.current == 1){
+                    end++;
+                }
+                if(args.current > args.pageCount-4 && args.current >= args.pageCount){
+                    start--;
+                }
+                for (;start <= end; start++) {
+                    if(start <= args.pageCount && start >= 1){
+                        if(start != args.current){
+                            obj.append('<a href="javascript:;" class="tcdNumber">'+ start +'</a>');
+                        }else{
+                            obj.append('<span class="current">'+ start +'</span>');
+                        }
+                    }
+                }
+                if(args.current + 2 < args.pageCount - 1 && args.current >= 1 && args.pageCount > 5){
+                    obj.append('<span>...</span>');
+                }
+                if(args.current != args.pageCount && args.current < args.pageCount -2  && args.pageCount != 4){
+                    obj.append('<a href="javascript:;" class="tcdNumber">'+args.pageCount+'</a>');
+                }
+                //涓嬩竴椤�
+                if(args.current < args.pageCount){
+                    obj.append('<a href="javascript:;" class="nextPage">&gt</a>');
+                    obj.append('<a href="javascript:;" class="end">末页</a>');
+                }else{
+                    obj.remove('.nextPage');
+                    obj.remove('.end');
+                    obj.append('<span class="disabled">&gt</span>');
+                    obj.append('<span class="disabled">末页</span>');
+                }
+                obj.append('<label class="jumplabel">跳转到<input type="text" id="selectcount" value=""/><a href="javascript:;" class="jumpgo">GO</a></label>');
+                //obj.append('');
+            })();
+        },
+        //缁戝畾浜嬩欢
+        bindEvent:function(obj,args){
+            return (function(){
+                obj.on("click","a.tcdNumber",function(){
+                    var current = parseInt($(this).text());
+                    ms.fillHtml(obj,{"current":current,"pageCount":args.pageCount});
+                    if(typeof(args.backFn)=="function"){
+                        args.backFn(current);
+                    }
+                });
+                //涓婁竴椤�
+                obj.on("click","a.start",function(){
+                    var current =1;
+                    ms.fillHtml(obj,{"current":1,"pageCount":args.pageCount});
+                    if(typeof(args.backFn)=="function"){
+                        args.backFn(1);
+                    }
+                });
+                obj.on("click","a.prevPage",function(){
+                    var current = parseInt(obj.children("span.current").text());
+                    ms.fillHtml(obj,{"current":current-1,"pageCount":args.pageCount});
+                    if(typeof(args.backFn)=="function"){
+                        args.backFn(current-1);
+                    }
+                });
+                //涓嬩竴椤�
+                obj.on("click","a.nextPage",function(){
+                    var current = parseInt(obj.children("span.current").text());
+                    ms.fillHtml(obj,{"current":current+1,"pageCount":args.pageCount});
+                    if(typeof(args.backFn)=="function"){
+                        args.backFn(current+1);
+                    }
+                });
+                obj.on("click","a.end",function(){
+                    var current =args.pageCount;
+                    ms.fillHtml(obj,{"current":current,"pageCount":current});
+                    if(typeof(args.backFn)=="function"){
+                        args.backFn(current);
+                    }
+                });
+                obj.on("click","a.jumpgo",function(){
+                    var current = parseInt($("#selectcount").val());
+                    if(current>0){
+                    ms.fillHtml(obj,{"current":current,"pageCount":args.pageCount});
+                    if(typeof(args.backFn)=="function"){
+                        args.backFn(current);
+                    }
+                    }
+                });
+            })();
+        }
+    }
+    $.fn.createPage = function(options){
+        var args = $.extend({
+            pageCount : 10,
+            current : 1,
+            backFn : function(){}
+        },options);
+        ms.init(this,args);
+    }
+})(jQuery)
