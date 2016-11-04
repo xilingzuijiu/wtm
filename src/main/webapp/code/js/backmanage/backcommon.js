526c5c730b534a4abe6c439b36b8c163b1ef377f
xilingzuijiu <xilingzuijiu@gmail.com>
2016-11-04 17:37:48 +0800

    F【pc站】商家后台页面发布任务重复提交

---
+++ src/main/webapp/code/js/backmanage/backcommon.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2016/11/4.
+ */
+document.write("<script language='javascript' src='../../code/js/jquery.min.js'></script>");
+document.write("<script language='javascript' src='../../code/js/jquery.cookie.js'></script>");
+document.write("<script language='javascript' src='../../code/js/bootstrap.min.js'></script>");
