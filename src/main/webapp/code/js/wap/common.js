4a29c3debde39d1f91337792df22379956f207dd
xilingzuijiu <xilingzuijiu@gmail.com>
2016-11-16 11:45:34 +0800

    F【pc站】js引入cdn链接

--- src/main/webapp/code/js/wap/common.js
+++ src/main/webapp/code/js/wap/common.js
@@ -1,10 +1,10 @@
 /**
  * Created by yuguodong on 2016/9/28.
  */
-
-document.write("<script language='javascript' src='../../code/js/jquery.min.js'></script>");
-document.write("<script language='javascript' src='../../code/js/jquery.cookie.js'></script>");
-document.write("<script language='javascript' src='../../code/js/bootstrap.min.js'></script>");
+document.write("<script language='javascript' src='http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js'></script>");
+!window.jQuery && document.write('<script src="../../code/js/jquery.min.js"><\/script>');
+document.write("<script language='javascript' src='http://apps.bdimg.com/libs/jquery.cookie/1.4.1/jquery.cookie.js'></script>");
+document.write("<script language='javascript' src='http://apps.bdimg.com/libs/bootstrap/3.3.4/js/bootstrap.min.js'></script>");
 document.write("<script language='javascript' src='http://api.map.baidu.com/api?v2.0&ak=V6aU35lWuOmpSj2nhoxaZza0rRsBoKQb'></script>");
 document.write("<script language='javascript' src='../../code/js/wap/index.js'></script>");
 
