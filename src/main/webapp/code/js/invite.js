2bf4570417aebcda096236b450437d76014c6fcd
xilingzuijiu <xilingzuijiu@gmail.com>
2016-11-23 10:36:42 +0800

    B[邀请]  邀请页注册下载

--- src/main/webapp/code/js/invite.js
+++ src/main/webapp/code/js/invite.js
@@ -1,6 +1,104 @@
+$(function(){
+    if (is_weixin()){
+            $(".bodybox").css("display","block");
+    } else {
+        document.body.innerHTML = "请在微信打开此页面"
+    }
+    $("#sendVerifyCode").click( function (){
+        var telephone = $("#telephone").val().trim();
+        if (telephone==null || !telephone.match(/^1\d{10}$/)){
+            alert("手机号码不正确")
+        }else {
+            $.ajax({
+                type: 'get',
+                url: '/pc/admin/member/sendIndentifyCode',
+                data: {telephone:telephone},
+                beforeSend: function (XMLHttpRequest) {
+                    getMemberRequestHeaderMsg(XMLHttpRequest)
+                } ,
+                success: function (params) {
+                    var data=eval(params)
+                    var errorCode=data.errorCode
+                    if (errorCode==0){
+
+                    } else if (errorCode==4){
+                        alert(data.message)
+                    }
+                }
+            });
+            $("#sendVerifyCode").css("display","none");
+            $("#downtime").css("display","block");
+            countDown();
+            setTimeout(function () {
+                $("#sendVerifyCode").css("display","block");
+                $("#downtime").css("display","none");
+            }, SS*1000);
+        }
+    });
+    $("#register").click( function (){
+        $("#form").attr("enctype","multipart/form-data");
+        var requestObj = eval($("#registerForm").serializeObject())
+        var request =getRegeisterParams(requestObj);
+        var telephone = $("#telephone").val().trim();
+        if(!$("#memberName").val()){
+            alert("用户名不能为空");
+        }else if($("#memberName").val().length>10){
+            alert("您输入的用户名过长");
+        }else if($("#password").val().length<6||$("#password").val().length>16){
+            alert("密码长度必须为6到16位");
+        }else if($("#password").val()!=$("#repassword").val()){
+            alert("密码不一致，请重新输入");
+        }else if(telephone==null || !telephone.match(/^1\d{10}$/)){
+            alert("手机号码不正确");
+        }else{
+            $("#register").attr('disabled', true);
+            $("#register").css('background', "#bebebe");
+            $.ajax({
+                type: 'post',
+                url: '/pc/admin/member/register',
+                dataType:"json",
+                data: request,
+                contentType: "application/json",
+                beforeSend: function (XMLHttpRequest) {
+                    getMemberRequestHeaderMsg(XMLHttpRequest)
+                } ,
+                success: function (params) {
+                    var data=eval(params)
+                    var errorCode=data.errorCode;
+                    if (errorCode==0){
+                        if (data.data){
+                            setTimeout(download(),5000);
+                        }
+                    } else if (errorCode==4){
+                        alert(data.message);
+                        $("#register").attr('disabled', false);
+                        $("#register").css('background', "#fa4a18");
+                    }
+                }, error:function(){
+                    alert(data.message);
+                    $("#register").attr('disabled', false);
+                    $("#register").css('background', "#fa4a18");
+                }
+            });
+        }
+    })
+});
+
+function getRegeisterParams(requestObj){
+    var invitedCode = requestObj.invitedCode
+    var identifyCode = requestObj.identifyCode
+    delete requestObj.invitedCode
+    delete requestObj.identifyCode
+    delete requestObj.repassword
+    var obj=new ReuqestObj(requestObj,invitedCode,identifyCode)
+    return JSON.stringify(obj)
+}
+
+
+
 function getinvitecode(){
     memberId=GetQueryString("memberId");
-    console.log("获取的的数据为："+memberId)
+    console.log("获取的的数据为："+memberId);
     $.ajax({
         type: 'get',
         dataType: 'json',
@@ -215,5 +313,13 @@ function getMemberRequestHeaderMsg(XMLHttpRequest){
         XMLHttpRequest.setRequestHeader("from",7);
     }else {
         XMLHttpRequest.setRequestHeader("from",2);
+    }
+}
+function is_weixin() {
+    var ua = navigator.userAgent.toLowerCase();
+    if (ua.match(/MicroMessenger/i) == "micromessenger") {
+        return true;
+    } else {
+        return false;
     }
 }
