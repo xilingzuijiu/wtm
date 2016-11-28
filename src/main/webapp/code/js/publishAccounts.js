50717e8ebaea8b57a16d1b28ec17db698db74e8c
xilingzuijiu <xilingzuijiu@gmail.com>
2016-11-28 09:50:11 +0800

    F[常见问题]  修改，js链接更换

--- src/main/webapp/code/js/publishAccounts.js
+++ src/main/webapp/code/js/publishAccounts.js
@@ -1,5 +1,5 @@
 document.write("<script language='javascript' src='../code/js/showBo.js'></script>");
-document.write("<script language='javascript' src='../code/js/jquery.min.js'></script>");
-document.write("<script language='javascript' src='../code/js/bootstrap.min.js'></script>");
+document.write("<script language='javascript' src='http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js'></script>");
+document.write("<script language='javascript' src='http://apps.bdimg.com/libs/bootstrap/3.3.4/js/bootstrap.min.js'></script>");
 document.write("<script language='javascript' src='../code/js/lrz.mobile.min.js'></script>");
 document.write("<script language='javascript' src='../code/js/publishTask.js'></script>");
--- src/main/webapp/code/js/sellerPage/paycashAccounts.js
+++ src/main/webapp/code/js/sellerPage/paycashAccounts.js
@@ -1,8 +1,8 @@
 /**
  * Created by Administrator on 2016/11/3.
  */
-document.write("<script language='javascript' src='../../code/js/jquery.min.js'></script>");
-document.write("<script language='javascript' src='../../code/js/jquery.cookie.js'></script>");
+document.write("<script language='javascript' src='http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js'></script>");
+document.write("<script language='javascript' src='http://apps.bdimg.com/libs/jquery.cookie/1.4.1/jquery.cookie.js'></script>");
 document.write("<script language='javascript' src='../../code/js/sellerPage/jquery.qrcode.js'></script>");
 document.write("<script language='javascript' src='../../code/js/sellerPage/qrcode.js'></script>");
 document.write("<script language='javascript' src='../../code/js/showBo.js'></script>");
